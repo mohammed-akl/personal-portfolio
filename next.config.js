19338df568c5ef6e4808faae3b72022b8f1031a2
Mohammed A <mohammedalukkal123@gmail.com>
2026-03-12 12:03:26 +0530

    revert /dark fix

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const path = require('path');
 
 const nextConfig = {
@@ -5,15 +6,6 @@ const nextConfig = {
   reactStrictMode: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'css')],
-  },
-  // This tells Next.js to serve the /dark content when the user hits /
-  async rewrites() {
-    return [
-      {
-        source: '/',
-        destination: '/dark',
-      },
-    ];
   },
 };
 
